43f1c7766d54ce04d146feec71429c3f1c91f785
Chungon <cotse@myseneca.ca>
2023-06-20 14:58:49 -0400

    trial

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # WEB3-Personnel-app-cyclic
 a reboot of my personnel app using cyclic
+https://puzzled-clam-attire.cyclic.app
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var upload = multer({storage: storage});
 
 onHttpStart = () => {
     console.log('Express http server listening on port ' + HTTP_PORT);
-}
+};
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -51,7 +51,7 @@ app.get("/employee/:employeeNum", (req,res) => {
         res.json({message: err});
     })
 });
-
+//employee funcs
 app.get("/employees", (req,res) => {
     if (req.query.status){
         dataService.getEmployeesByStatus(req.query.status)
@@ -93,27 +93,28 @@ app.get("/images", (req,res) => {
 });
 //manager func
 app.get("/managers", (req,res) => {
-    dataService.getManagers()
-    .then((data) => res.json(data))
-    .catch((err) => res.json({error: 'message'}))
+    dataService.getManagers().then((data) => {
+        res.json({data});
+    }).catch((err) => {
+        res.json({message: err});
+    })
 });
 //dept func
 app.get("/departments", (req,res) => {
-    dataService.getDepartments()
-    .then((data) => res.json(data))
-    .catch((err) => res.json({error: 'message'}))
+    dataService.getDepartments().then((data) => {
+        res.json({data});
+    }).catch((err) => {
+        res.json({message: err});
+    })
 });
 
 app.get('*', (req, res) => {
     res.status(404).send("Page Not Found");
     res.redirect("https://miro.medium.com/max/1400/1*BY3XOmM2egjm3LIfRjqZiw.png");
-  });
+});
 
-
-dataService.initialize()
-.then((data) =>{
+dataService.initialize().then((data) =>{
     app.listen(HTTP_PORT, onHttpStart());    
-})
-.catch(() => {
+}).catch(() => {
     console.log("There was an error with initializing.");
-})
+});
